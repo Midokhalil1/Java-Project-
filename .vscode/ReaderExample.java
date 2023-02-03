7508ad9f93457396a15163c74ed96cfb920f68be
Midokhalil1 <midokhalil22@gmail.com>
2023-02-03 11:48:57 -0500

    Readerproject

--- .vscode/ReaderExample.java
+++ .vscode/ReaderExample.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class ReaderExample {
     public static void main(String[] args) {
         
+        Scanner reader = new Scanner(System.in);
+        String name;
+        byte age;
+
+
 
         System.out.println("Enter your name");
+        name = reader.nextLine();
         System.out.println("Enter your age");
+        age = reader.nextByte();
+
+        System.out.println("Hello "  +  name +  "your age is :  " + age);
     }
 }
