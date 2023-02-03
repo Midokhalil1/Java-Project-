96eae52bcc9d6a5cfc8a8763d282c510c49e625d
Midokhalil1 <midokhalil22@gmail.com>
2023-02-03 11:09:36 -0500

    reader

---
+++ .vscode/ReaderExample.java
@@ -0,0 +1,8 @@
+public class ReaderExample {
+    public static void main(String[] args) {
+        
+
+        System.out.println("Enter your name");
+        System.out.println("Enter your age");
+    }
+}
