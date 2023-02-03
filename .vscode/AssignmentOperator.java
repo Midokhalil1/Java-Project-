1fa2e24730ae1d83d97cdb21c09006a7f3ef7ca7
Midokhalil1 <midokhalil22@gmail.com>
2023-02-03 10:52:23 -0500

    devisonOperator

--- .vscode/AssignmentOperator.java
+++ .vscode/AssignmentOperator.java
@@ -1,8 +1,11 @@
 public class AssignmentOperator {
     public static void main(String[] args) {
         int a = 5 ;
+        String b = "Hello World";
+        a = 7;
      
         System.out.println(a);
+        System.out.println(b);
     
     }
     
---
+++ .vscode/DevisonOperator.java
@@ -0,0 +1,11 @@
+public class DevisonOperator {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int c = a / b;
+        int s = a % b;
+
+        System.out.println(c);
+        System.out.println(s);
+    }
+}
