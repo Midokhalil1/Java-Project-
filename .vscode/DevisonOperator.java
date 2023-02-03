8a9a6b1a65723c44f97ccba7b1cda418a51f7493
Midokhalil1 <midokhalil22@gmail.com>
2023-02-03 10:55:56 -0500

    float

--- .vscode/DevisonOperator.java
+++ .vscode/DevisonOperator.java
@@ -1,9 +1,9 @@
 public class DevisonOperator {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 3;
-        int c = a / b;
-        int s = a % b;
+        float a = 5;
+        float b = 3;
+        float c = a / b;
+        float s = a % b;
 
         System.out.println(c);
         System.out.println(s);
