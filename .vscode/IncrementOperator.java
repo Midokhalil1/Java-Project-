b8c0b6950b58583a9592745deaddd1b90b840f19
Midokhalil1 <midokhalil22@gmail.com>
2023-02-03 10:59:46 -0500

    IncrementOperator

---
+++ .vscode/IncrementOperator.java
@@ -0,0 +1,12 @@
+public class IncrementOperator {
+    public static void main(String[] args) {
+        int a = 5;
+        a++;
+        int b = 3;
+        b--;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
